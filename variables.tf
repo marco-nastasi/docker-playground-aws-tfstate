67d6486f6aa8fc636260da79d576cc9a09f698cd
Marco Antonio Nastasi <mnastasi@gmail.com>
2025-01-11 18:13:45 +0100

    Formatted variables file

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable name {
-    description = "Variable containing the project name"
-    type = string
+variable "name" {
+  description = "Variable containing the project name"
+  type        = string
 }
 
 variable "tags" {
   description = "Map containing the tags of the S3 bucket and the DynamoDB table"
-  type = map(string)
+  type        = map(string)
 }
