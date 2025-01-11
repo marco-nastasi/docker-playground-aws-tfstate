068432c6b72f852dfef133e5d00cacd03da8fff4
Marco Antonio Nastasi <mnastasi@gmail.com>
2025-01-11 18:22:52 +0100

    Fixed outputs and reference to variables

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "docker_playground_tfstate" {
   source = "github.com/marco-nastasi/terraform-aws-s3-dynamodb-state"
 
   project_name = var.name
-  common_tags  = tags
+  common_tags  = var.tags
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "module_output_s3_bucket_ids" {
-  value = module.test_project1_state.s3_bucket_id
+  value = module.docker_playground_tfstate.s3_bucket_id
 }
 
 output "module_output_dynamodb_table_names" {
-  value = module.test_project1_state.dynamodb_table_name
+  value = module.docker_playground_tfstate.dynamodb_table_name
 }
